739fd73a97ce7b652fb571ae58b013947f279704
CalmaxBenard <calmaxbenard@gmail.com>
2024-05-05 16:02:24 +0300

    alter about css

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './About.css'
-import about_img from '../../assets/about.png'
+import about_img from '../../assets/jabuya.JPG'
 import play_icon from '../../assets/play-icon.png'
 
 const About = ({setPlayState}) => {
@@ -14,6 +14,16 @@ const About = ({setPlayState}) => {
             <h3>ABOUT CRIDON PINNACLE</h3>
             <h2>Nurturing Tomorrow's Leaders Today</h2>
             <p>
+                Cridon Pinnacle Tuition Network (CPTN) believes in making high-quality education accessible to learners of 
+                all backgrounds. We believe that quality education is the foundation to a successful society. 
+                The tuition network was founded in 2020, at a time when the Covid-19 was ravaging the world.                
+            </p>
+            <p>
+                CPTN, amidst the challenges posed by the pandemic, emerged as a fortress of hope for the learners 
+                who direly sought effective learning solutions in the ever-changing educational landscape. 
+                We have an unwavering commitment to excellence, and for this, we are trusted by many in the education industry. 
+            </p>
+            <p>
                 Embark on a transfomative educational journey with our Cridon Pinnacle 
                 comprehensive tuition programs. Our cutting-edge curriculum is designed
                 to offer student-centred experience to empower students with the necessary
@@ -24,11 +34,7 @@ const About = ({setPlayState}) => {
                 our programs prepar aspiring educators to make a meaningful impact in 
                 classrooms, schools, and job industry.
             </p>
-            <p>
-                Whether aspiring to become a teacher, administrator, counselor, or educational
-                leader, our diverse range of programs offers the perfect pathway to achieve 
-                your goals and unlock your full potential in shaping the future of education.
-            </p>
+    
         </div>
     </div>
   )
